a1b1eb2ae92e553d8f803efd0c8cfdbb8972def1
root <root@ip-10-0-0-154.ec2.internal>
2024-11-09 21:16:46 +0000

    I have update some vpc components

--- createInstance.tf
+++ createInstance.tf
@@ -15,6 +15,7 @@ resource "aws_subnet" "subnet1" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
   availability_zone = "us-east-2a" 
+  map_public_ip_on_launch = true
   tags = {
     Name = "subnet1"
   }
@@ -24,11 +25,38 @@ resource "aws_subnet" "subnet2" {
   vpc_id = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
   availability_zone = "us-east-2b"
+  map_public_ip_on_launch = true
   tags = {
     Name = "subnet2"
   }
 }
 
+# Create Internet Gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "lucky-igw"
+  }
+}
+# Create route table
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "lucky-route-table"
+  }
+}
+# Associate route table with subnets
+resource "aws_route_table_association" "rta" {
+  count      = 2
+  subnet_id  = aws_subnet.subnet1.id
+  subnet_id  = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.rt.id
+}
 # Create a security group
 resource "aws_security_group" "instance_sg" {
   vpc_id = aws_vpc.main.id
