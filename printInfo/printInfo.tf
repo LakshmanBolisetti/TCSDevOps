150ab8c4863f472a857915ed0c017b391bb6a0cd
root <root@ip-10-0-0-154.ec2.internal>
2024-11-09 20:52:42 +0000

    It will prints the Instances and security groups data

---
+++ printInfo/printInfo.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  region = "us-east-2" # Replace with your region
+}
+
+# Data source to retrieve all EC2 instances
+data "aws_instances" "all" {}
+
+# Loop through each instance to get detailed information
+data "aws_instance" "details" {
+  for_each = toset(data.aws_instances.all.ids)
+  instance_id = each.value
+}
+# Output block to print instance IDs
+output "ec2_instance_ids" {
+value = data.aws_instances.all.ids
+}
+
+# Output block to print instance IDs and associated security groups
+output "ec2_instances_and_security_groups" {
+  value = {
+    for id, instance in data.aws_instance.details:
+    id => instance.vpc_security_group_ids
+  }
+}
